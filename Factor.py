19026fa2c4bcadf2b8a3de1a1be130db68d4a6e6
indapa <indapa@gmail.com>
2012-09-29 18:39:41 -0400

    initial commit

---
+++ Factor.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+class Factor(object):
+    
+
+    def __init__(self, var=[], card=[], val=[]):
+        self.var= np.array(var)
+        self.card=np.array(card)
+        self.val=np.array(val)
+
+    def __str__(self):
+        varstring= " ".join ( map(str, self.var) )
+        cardstring=" ".join ( map(str, self.card) )
+        valstring= " ".join( map(str, self.val))
+        return "\n".join( [ varstring, cardstring, valstring])
+
+
+    def setVar(self, var):
+        self.var=np.array(var)
+
+    def getVar(self):
+        return self.var
+
+    def setVal(self, val):
+        self.val=np.array(val)
+
+    def getVal(self):
+        return self.val
+
+    def setCard(self,card):
+        self.card=np.array(card)
+
+    def getCard(self):
+        return self.card
+
+
+
+
+
