4263d7428a3a8ffe49916ef7ef9d2ba3c2420c87
indapa <indapa@gmail.com>
2012-09-30 18:51:18 -0400

    finished IndexToAssignment

--- FactorOperations.py
+++ FactorOperations.py
@@ -5,9 +5,18 @@ import numpy as np
 def IndexToAssignment( I, D):
     a=np.reshape ( np.arange(np.prod(D)).repeat(len(D)), (np.prod(D),len(D)))
     #print np.shape(a)
-    print a
+    #print a
 
     b=tmp=list( D[:-1] )
     tmp.insert(0,1)
     tmp =np.cumprod ( np.array (tmp) )
-    print tmp
+    b=np.tile( np.cumprod(b), (len(I), 1))
+    #print b
+
+    #print np.floor ( a /b )
+    c=np.tile ( D, ( len(I), 1) )
+
+    assignment = np.mod ( np.floor( a/b), c)  +1
+    return assignment
+
+    #repmat(cumprod([1, D(1:end - 1)]), length(I), 1)
