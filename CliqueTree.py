95b2a09302d990f92edf7b8568febda002bd87ff
indapa <indapa@gmail.com>
2012-11-01 12:13:10 -0400

    eliminateVar function: elimination algorithm

--- CliqueTree.py
+++ CliqueTree.py
@@ -1,36 +1,60 @@
-from Factor import *
-from PGMcommmon import *
-from PedigreeFactors import *
-import numpy as np
-
-
 class CliqueTree(object):
-    """ represent a CliqueTree (Junction Tree)
-        a clique tree is a graph whose nodes are cliques
-        has an adjacency matrix and a factorList from which
-        the graph was built from
-        a clique is associated with a subset of variables
-        an edge connects to cliques is associated with the
-        sepset (intersection of variables between) the  cliques"""
+    'MyClass constructor'
 
-    def __init__(self, nodeList=[], edges=[], factorList=[]):
+    def __init__(self, nodeList=[], edges=[], factorList=[],evidence=[]):
         self.nodeList=nodeList
         self.edges=edges
         self.factorList=factorList
+        self.evidence=evidence
 
     def setNodeList(self,nodeList):
         self.nodeList=nodeList
+
     def setEdges(self,edges):
         self.edges=edges
+
     def setFactorList(self,factorList):
         self.factorList=factorList
 
+    def setEvidence(self,evidence):
+        self.evidence=evidence
+
     def getNodeList(self):
         return self.nodeList
+
     def getEdges(self):
         return self.edges
+
     def getFactorList(self):
         return self.factorList
 
+    def getEvidence(self):
+        return self.evidence
 
+    def eliminateVar(self,Z):
+        """ a variable elimination function
+            based on https://github.com/indapa/PGM/blob/master/Prog4/EliminateVar.m """
+
+        useFactors = []#the index of the factor that contains the variable Z
+        scope = []
+
+        for i in range (len(self.factorList)):
+            if Z in self.factorList[i].getVar().tolist():
+                useFactors.append(i)
+                scope=list(set.union(set(scope), self.factorList[i].getVar().tolist() ))
+
+        print useFactors
+        print scope
+
+        # update edge map
+        # These represent the induced edges for the VE graph.
+
+        for i in range ( len(scope)):
+            for j in range ( len(scope)):
+                if i!=j:
+                    self.edges[i,j]=1
+                    self.edges[j,i]=1
+        self.edges[Z,:]=0
+        self.edges[:,Z]=0
+        
 
