400fa0890af8a96d3b35689c72a68ad73f2f0916
indapa <indapa@gmail.com>
2012-12-03 12:27:40 -0500

    initial commit; uses test data from matlab file

---
+++ PythonNotebooks/testCliqueTreeCalibrate.py
@@ -0,0 +1,22 @@
+from Factor import *
+from PGMcommon import *
+from CliqueTree import *
+from CliqueTreeOperations import *
+from FactorOperations import *
+import scipy.io as sio
+import numpy as np
+import pprint
+matfile='/Users/amit/BC_Classes/PGM/Prog4/PA4Sample.mat'
+mat_contents=sio.loadmat(matfile)
+mat_struct=mat_contents['SumProdCalibrate']
+val=mat_struct[0,0]
+input_edges = val['INPUT']['edges'][0][0]
+input_cliqueList= val['INPUT']['cliqueList'][0][0][0]
+clique_list_factorObj=[]
+for tpl in input_cliqueList:
+    (var, card, values)=tpl
+    f= Factor( var[0].tolist(), card[0].tolist(), values[0].tolist(), 'factor' )
+    clique_list_factorObj.append(f)
+
+P=CliqueTree( clique_list_factorObj ,  input_edges, clique_list_factorObj, [])
+CliqueTreeCalibrate(P)
