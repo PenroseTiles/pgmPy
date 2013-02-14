ea8c57826f68a1448060adb7514e034e9ebcd3e4
indapa <indapa@gmail.com>
2013-02-14 16:49:21 -0500

    initial commit; testing max-sum MAP assignment with OCR test data

---
+++ PythonNotebooks/ocr_MAP.py
@@ -0,0 +1,35 @@
+from Factor import *
+from PGMcommon import *
+from CliqueTree import *
+from CliqueTreeOperations import *
+from FactorOperations import *
+import scipy.io as sio
+import numpy as np
+import pprint
+import pdb
+matfile='/Users/amit/BC_Classes/PGM/Prog4/PA4Sample.mat'
+mat_contents=sio.loadmat(matfile)
+mat_struct=mat_contents['OCRNetworkToRun']
+val=mat_struct
+factorList=[]
+
+ALPHABET=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for data in val:
+    
+    (var, card, values)=data[0]
+    #print var, card, values
+    f= Factor( var[0].tolist(), card[0].tolist(), values[0].tolist(), 'factor' )
+    #print f
+    factorList.append( f )
+
+
+MARGINALS= ComputeExactMarginalsBP( factorList, [], 1 )
+
+#for m in MARGINALS:
+#    print m
+#    print 
+
+MAPAssignment=MaxDecoding( MARGINALS )
+
+print "".join( [ALPHABET[idx] for idx in MAPAssignment] )
